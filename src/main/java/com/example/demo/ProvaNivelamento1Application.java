697dfe6dbd53412e92d4440bbfa1abb0842ce6a1
Yuri Trierveiler <yuri.trier@gmail.com>
2021-03-25 00:09:19 -0300

    Projeto criado

---
+++ src/main/java/com/example/demo/ProvaNivelamento1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaNivelamento1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaNivelamento1Application.class, args);
+	}
+
+}
