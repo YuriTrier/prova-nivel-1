20ce96349bb025688cde1df750d202a934dc0310
Yuri Trierveiler <yuri.trier@gmail.com>
2021-03-25 12:56:10 -0300

    Criacao ItemPedido e vinculo entre entidades

---
+++ src/main/java/com/yuritrier/entites/pk/ItemPedidoPK.java
@@ -0,0 +1,67 @@
+package com.yuritrier.entites.pk;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.yuritrier.entites.Item;
+import com.yuritrier.entites.Pedido;
+
+@Embeddable
+public class ItemPedidoPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "pedido_id")
+	private Pedido pedido;
+	
+	@ManyToOne
+	@JoinColumn(name = "item_id")
+	private Item item;
+	
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((item == null) ? 0 : item.hashCode());
+		result = prime * result + ((pedido == null) ? 0 : pedido.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedidoPK other = (ItemPedidoPK) obj;
+		if (item == null) {
+			if (other.item != null)
+				return false;
+		} else if (!item.equals(other.item))
+			return false;
+		if (pedido == null) {
+			if (other.pedido != null)
+				return false;
+		} else if (!pedido.equals(other.pedido))
+			return false;
+		return true;
+	}
+	
+}
